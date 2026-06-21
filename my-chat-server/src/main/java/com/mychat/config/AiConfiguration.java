6e4197542d2d5a8a7d96fc7b4a1b914c5e904355
All1217 <2625705206@qq.com>
2026-06-21 23:30:11 +0800

    尝试添加RAG功能

--- my-chat-server/src/main/java/com/mychat/config/AiConfiguration.java
+++ my-chat-server/src/main/java/com/mychat/config/AiConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
